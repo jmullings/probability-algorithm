0905cae3f2b9dbe41e2aaceb69697be954e027dc
Jason Mulings <j_mullings@hotmail.co.uk>
2015-12-28 00:48:29 +1300

    probability.algorithm.handler
    reviewed makeRecursive...

--- probability.algorithm.handler.php
+++ probability.algorithm.handler.php
@@ -9,83 +9,62 @@ include_once 'bighugelabs_class.php';
 $synonyms = new BigHugeLabs();
 $keywords = "test";
 
-class probabilityRandom {
-	#private vars
-	var
-		$data = array(),
-		$universe = 0;
-	#flattern array	
-	function makeRecursive($array =""){
-		$array = call_user_func_array('array_merge_recursive', $array);
-		foreach($array as $value){
-		if(is_array($value))
-		return makeRecursive($array);
-		else
-		$rec_array []= $value;
-		}	
-		return $rec_array;
-	}	
-
-	#add an item to the list and defines its probability of beeing chosen
-	function add( $data, $probability ){
-		$this->data[ $x = sizeof( $this->data ) ] = new stdClass;
-		$this->data[ $x ]->value = $data;
-		$this->universe += $this->data[ $x ]->probability = abs( $probability );
-	}
-
-	#remove an item from the list
-	function remove( $index ){
-		if( $index > -1 && $index < sizeof( $this->data ) ) {
-			$item = array_splice( $this->data, $index, 1 );
-			$this->universe -= $item->probability;
-		}
-	}
-
-	#clears the class
-	function clear(){
-		$this->universe = sizeof( $this->data = array() );
-	}
-
-	#return a randomized item from the list
-	function get(){
-		if( !$this->universe )
-			return null;
-		$x = round( mt_rand( 0, $this->universe ) );
-		$max = $i = 0;
-		do
-			$max += $this->data[ $i++ ]->probability;
-		while( $x > $max );
-		return $this->data[ $i-1 ]->value;
-	}
-}
-
-$prExample = new probabilityRandom;
-/*
-$array = $prExample->makeRecursive($synonyms-> getKeywordArray($keywords));
-
-///Can be repeated for wider scope
-foreach($array as $word){
-	
-	$secondary_array []= $synonyms-> getKeywordArray($word);
-	
-	}
-
-foreach($prExample->makeRecursive($secondary_array) as $test_array)
-{
-	
-	echo $test_array."<br />";
-	
-};
+class probabilityMatrix
+ {
+   #private vars
+   var 
+   $prob_array = array(), 
+   $p_array = array();
+   
+   #flatten each array
+   public function makeRecursive( $ar = "" )
+   {
+     $toflat = array(
+        $ar 
+     );
+     $res    = array();
+     while ( ( $r = array_shift( $toflat ) ) !== NULL ) {
+       foreach ( $r as $v ) {
+         if ( is_array( $v ) ) {
+           $toflat[] = $v;
+         } else {
+           $res[] = $v;
+         }
+       }
+     }
+     return $res;
+   }
+   #Asort by probability
+   public function returnProbability( $array = array())
+   {
+	 $array  = $this->makeRecursive( $array );  
+     $this->p_array = array_count_values( $array );
+	 
+     foreach ( $this->p_array as $words => $val ) {
+       $this->prob_array[$words] = ($val/count( array_unique( $array ) )) * 100;
+     }
+     
+     arsort( $this->prob_array );
+     return $this->prob_array;
+   }
+ }
+ 
+ 
+ $pm    = new probabilityMatrix;
+ /* SKIP FOR EASE
+ $array = $pm->makeRecursive( $synonyms->getKeywordArray( $keywords ) );
+ 
+ foreach ( $array as $word ) {
+   $secondary_array[] = $synonyms->getKeywordArray( $word );
+ }
+ 
+ $test_array = array_merge( $array,  $secondary_array  );
+ 
+ 
 */
+
 include_once 'test.word.array.php';
 
 global $test_array;
 
-foreach(array_count_values($test_array) as $prob_words => $val)
-{
-	
-	$prExample->add($prob_words, $val*100 );
-	
-};
-#Possible adjustment for array / prob ratio and inclusion to class!!
-for( $x=10; $x--; print strtolower( $prExample->get() ). '<br />' );
+print_r( $pm->returnProbability( $test_array ) );
